fa8cfaed8d9b83cc10246beac602339ddc5fd7a4
ivasio <van.chebotar@yandex.ru>
2020-09-20 06:09:34 +0300

    added actually tile drawing and reformatted the code

--- webapp-i1/app/static/js/map.js
+++ webapp-i1/app/static/js/map.js
@@ -1,101 +1,128 @@
 // Zurich
-var startlat = 47.38625170;
-var startlon = 8.52872890;
+let startlat = 47.38625170;
+let startlon = 8.52872890;
 
-var options = {
- center: [startlat, startlon],
- zoom: 14
+let options = {
+    center: [startlat, startlon],
+    zoom: 14
 }
 
 document.getElementById('lat').value = startlat;
 document.getElementById('lon').value = startlon;
 
-var map = L.map('map', options);
-var nzoom = 14;
+let map = L.map('map', options);
+let nzoom = 14;
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: 'OSM'}).addTo(map);
 
-var myMarker = L.marker([startlat, startlon], {title: "Coordinates", alt: "Coordinates", draggable: true}).addTo(map).on('dragend', function() {
- var lat = myMarker.getLatLng().lat.toFixed(8);
- var lon = myMarker.getLatLng().lng.toFixed(8);
- var czoom = map.getZoom();
- if(czoom < 18) { nzoom = czoom + 2; }
- if(nzoom > 18) { nzoom = 18; }
- if(czoom != 18) { map.setView([lat,lon], nzoom); } else { map.setView([lat,lon]); }
- document.getElementById('lat').value = lat;
- document.getElementById('lon').value = lon;
- myMarker.bindPopup("Lat " + lat + "<br />Lon " + lon).openPopup();
+let myMarker = L.marker([startlat, startlon], {
+    title: "Coordinates",
+    alt: "Coordinates",
+    draggable: true
+}).addTo(map).on('dragend', function () {
+    let lat = myMarker.getLatLng().lat.toFixed(8);
+    let lon = myMarker.getLatLng().lng.toFixed(8);
+    let czoom = map.getZoom();
+    if (czoom < 18) {
+        nzoom = czoom + 2;
+    }
+    if (nzoom > 18) {
+        nzoom = 18;
+    }
+    if (czoom !== 18) {
+        map.setView([lat, lon], nzoom);
+    } else {
+        map.setView([lat, lon]);
+    }
+    document.getElementById('lat').value = lat;
+    document.getElementById('lon').value = lon;
+    myMarker.bindPopup("Lat " + lat + "<br />Lon " + lon).openPopup();
 });
 
-function chooseAddr(lat1, lng1)
-{
- myMarker.closePopup();
- map.setView([lat1, lng1],18);
- myMarker.setLatLng([lat1, lng1]);
- lat = lat1.toFixed(8);
- lon = lng1.toFixed(8);
- document.getElementById('lat').value = lat;
- document.getElementById('lon').value = lon;
- myMarker.bindPopup("Lat " + lat + "<br />Lon " + lon).openPopup();
-}
 
-function myFunction(arr)
-{
- var out = "<br />";
- var i;
-
- if(arr.length > 0)
- {
-  for(i = 0; i < arr.length; i++)
-  {
-   out += "<div class='address' title='Show Location and Coordinates' onclick='chooseAddr(" + arr[i].lat + ", " + arr[i].lon + ");return false;'>" + arr[i].display_name + "</div>";
-  }
-  document.getElementById('results').innerHTML = out;
- }
- else
- {
-  document.getElementById('results').innerHTML = "Sorry, no results...";
- }
-
+function chooseAddr(lat1, lng1) {
+    myMarker.closePopup();
+    map.setView([lat1, lng1], 18);
+    myMarker.setLatLng([lat1, lng1]);
+    let lat = lat1.toFixed(8);
+    let lon = lng1.toFixed(8);
+    document.getElementById('lat').value = lat;
+    document.getElementById('lon').value = lon;
+    myMarker.bindPopup("Lat " + lat + "<br />Lon " + lon).openPopup();
 }
 
 
-// Backend requesting and tile drawing starts here ...
+function myFunction(arr) {
+    let out = "<br />";
+    let i;
+
+    if (arr.length > 0) {
+        for (i = 0; i < arr.length; i++) {
+            out +=
+                "<div class='address' " +
+                "title='Show Location and Coordinates' " +
+                "onclick='chooseAddr(" + arr[i].lat + ", " + arr[i].lon + "); return false;'" +
+                ">" + arr[i].display_name + "</div>";
+        }
+        document.getElementById('results').innerHTML = out;
+    } else {
+        document.getElementById('results').innerHTML = "Sorry, no results...";
+    }
+
+}
+
+function addr_search() {
+    let inp = document.getElementById("addr");
+    let xmlhttp = new XMLHttpRequest();
+    let url = "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + inp.value;
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let myArr = JSON.parse(this.responseText);
+            myFunction(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+
 const backend_url_base = 'http://165.232.120.34:8000'
 
-function draw_tile(tile) {
- console.log(tile.lower_left.x, tile.anxiety)
+let tiles_polygons = []
+function draw_tile(lat, lon, tile) {
+    let layer_removed = (!tiles_polygons.length);
+    console.log(layer_removed)
+    for (let tile_polygon of tiles_polygons) {
+        const old_tile = tile_polygon[0]
+        if (!(lat >= old_tile.lower_left.x && lat <= old_tile.upper_right.x &&
+                lon >= old_tile.lower_left.y && lon <= old_tile.upper_right.y)) {
+            layer_removed = true
+            map.removeLayer(tile_polygon[1])
+        }
+    }
+
+    if (layer_removed) {
+        const latlngs = [
+            [tile.lower_left.x, tile.lower_left.y],
+            [tile.lower_left.x, tile.upper_right.y],
+            [tile.upper_right.x, tile.upper_right.y],
+            [tile.upper_right.x, tile.lower_left.y],
+        ];
+        const polygon = L.polygon(latlngs, {color: 'red'}).addTo(map)
+        tiles_polygons = [[tile, polygon]]
+    }
 }
 
 function onMapClick(e) {
- const lat = e.latlng.lat;
- const lon = e.latlng.lng;
+    const lat = e.latlng.lat;
+    const lon = e.latlng.lng;
 
- fetch(`${backend_url_base}/point/${lat}/${lon}/predict`)
-  .then(response => response.json().then(tiles => {
-   for (let tile of tiles)
-    draw_tile(tile)
-  }))
-
+    fetch(`${backend_url_base}/point/${lat}/${lon}/predict`)
+        .then(response => response.json().then(tiles => {
+            for (let tile of tiles)
+                draw_tile(lat, lon, tile)
+        }))
 }
 
 map.on('click', onMapClick)
-// ... and ends here
-
-function addr_search()
-{
- var inp = document.getElementById("addr");
- var xmlhttp = new XMLHttpRequest();
- var url = "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + inp.value;
- xmlhttp.onreadystatechange = function()
- {
-   if (this.readyState == 4 && this.status == 200)
-   {
-    var myArr = JSON.parse(this.responseText);
-    myFunction(myArr);
-   }
- };
- xmlhttp.open("GET", url, true);
- xmlhttp.send();
-}
-
--- webapp-i1/app/views.py
+++ webapp-i1/app/views.py
@@ -1,30 +1,29 @@
 from app import app
-from flask import render_template, request, redirect, jsonify, make_response, session
-from datetime import datetime
-#import image_slicer
-import os
-#import numpy
-from werkzeug.utils import secure_filename
+from flask import render_template, request, redirect, jsonify, make_response
+
 
 @app.template_filter('clean_date')
 def clean_date(dt):
     return dt.strftime('%d %b %Y')
- 
+
+
 @app.route('/')
 def index():
     return render_template('public/index.html')
+
 
 @app.route('/map')
 def map():
     return render_template('public/map2.html')
 
+
 @app.route('/draw')
 def draw():
     return render_template('public/full.html')
 
+
 @app.route('/login', methods=["GET", "POST"])
 def sign_up():
-
     if request.method == "POST":
         req = request.form
         username = req["username"]
@@ -37,23 +36,20 @@ def sign_up():
     return render_template('public/login.html')
 
 
-
-
 @app.route("/json", methods=["POST"])
 def json():
     if request.is_json:
         req = request.get_json()
         response = {
-            "message":"JSON Received!",
+            "message": "JSON Received!",
             "name": req.get("name")
         }
         res = make_response(jsonify(response), 200)
         return res
     else:
-        res = make_response(jsonify({"message":"No JSON received"}), 400)
+        res = make_response(jsonify({"message": "No JSON received"}), 400)
         return res
 
-app.config["IMAGE_UPLOADS"] = "./app/static/img/uploads/" 
+
+app.config["IMAGE_UPLOADS"] = "./app/static/img/uploads/"
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", " GIF"]
-
-
