f62ca92e5590216fa5c05c6de0c40f93de84c32f
ivasio <van.chebotar@yandex.ru>
2020-09-20 08:46:05 +0300

    added inference mock

--- backend/app/api/inference.py
+++ backend/app/api/inference.py
@@ -1,14 +1,24 @@
 import asyncio
+import pickle
 from typing import List
 
+import pandas as pd
+import xgboost as xgb
+
 from ..models import Point, TilePrediction
-# from ..db import get_features
+from ..db import get_features
+
+
+with open('app/model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 
 async def predict_density(x_coord: float, y_coord: float) -> List[TilePrediction]:
-    # features = await get_features()
+    data = pd.DataFrame(await get_features())
+    # print(X_test)
+    score = model.predict(data)[0]
     request = Point(x=x_coord, y=y_coord)
-    tile_size = Point(x=0.0013, y=0.0033)
+    tile_size = Point(x=0.0006719612516592477, y=0.00064319500257117284)
     return [
-        TilePrediction.construct_adjusted(request - tile_size, request + tile_size, 75)
+        TilePrediction.construct_adjusted(request - tile_size, request + tile_size, score)
     ]
--- backend/app/db.py
+++ backend/app/db.py
@@ -1,13 +1,17 @@
-import asyncio
-from typing import List, Tuple, Callable
+# import asyncio
+# from typing import List, Tuple, Callable
+#
+# from sqlalchemy.dialects.postgresql import JSONB
+# from pydantic import parse_obj_as
+# from gino import Gino
+#
+# # from .models import ...
+#
+# db = Gino()
 
-from sqlalchemy.dialects.postgresql import JSONB
-from pydantic import parse_obj_as
-from gino import Gino
-
-# from .models import ...
-
-db = Gino()
+async def get_features():
+    return {'temp_3': [19], 'prec_3': [2], 'temp_2': [20], 'prec_2': [0], 'temp_1': [17], 'prec_1': [3], 'articl': [2144], 'perc_rel': [0.310168],
+	'perc_zur': [0.018190], 'trips': [5739], 'incoming': [1437], 'inside': [1944]}
 
 # async def get_features() -> Tuple[
 #     Tuple[temp, prec, desc] (-3, -2, -1),
